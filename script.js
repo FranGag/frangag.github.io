353a3fd432e3371f37d4a2f65575fad54667b4ff
Franco <francogaglianopanzardi@gmail.com>
2022-08-09 03:15:02 -0300

    fix

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function func() {
+
+/* Acá va una función que muestre un mensaje cuando el usuario hace click en el link a mi página web
+ function func() {
     var x = document.getElementById("alert");
     x.style.display = "none";
     if (x.style.display === "none") {
